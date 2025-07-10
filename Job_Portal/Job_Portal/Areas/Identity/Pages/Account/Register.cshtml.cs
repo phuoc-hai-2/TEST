13614cae129f5ccaa0fcdb2959efba6dfad98ec1
phuoc-hai-2 <daophuochai2004@gmail.com>
2025-07-10 14:01:25 +0700

    update 1.0.1

--- Job_Portal/Job_Portal/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Job_Portal/Job_Portal/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,6 +120,7 @@ namespace Job_Portal.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.FullName = Input.FullName; // <--- THÊM DÒNG NÀY
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
